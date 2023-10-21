51ea8a52ea890607ecfe8d55b10fe1236b106761
Josue Tejeda <josuetejeda02@gmail.com>
2023-10-21 12:31:14 -0500

    dependency inversion

--- 5-DependencyInversion/Api/Controllers/StudentController.cs
+++ 5-DependencyInversion/Api/Controllers/StudentController.cs
@@ -5,8 +5,14 @@ namespace DependencyInversion.Controllers;
 [ApiController, Route("student")]
 public class StudentController : ControllerBase
 {
-    StudentRepository studentRepository = new StudentRepository();
-    Logbook logbook = new Logbook();
+    IStudentRepository studentRepository;
+    ILogbook logbook;
+
+    public StudentController(IStudentRepository stundent, ILogbook log)
+    {
+        studentRepository = stundent;
+        logbook = log;
+    }
 
     [HttpGet]
     public IEnumerable<Student> Get()
